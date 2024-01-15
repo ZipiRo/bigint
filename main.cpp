5fcb2fe285584c75cf82b99dcb30d620ed3df45c
ZipiRo <143938588+ZipiRo@users.noreply.github.com>
2024-01-15 18:07:13 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "bigint.h" 
+
+using namespace std;
+
+int main(){
+    system("color b");
+    
+    bint a, b;
+
+    cin >> a >> b;
+
+    system("cls & color c");
+
+    cout << a << " - " << b << " = " << a - b << '\n';
+    cout << a << " + " << b << " = " << a + b << '\n';
+    cout << a << " * " << b << " = " << a * b << '\n';
+
+    cout << '\n'  << a << " < " << b;
+    a < b ? cout << " -> True" : cout << " -> False";
+
+    cout << '\n' << a << " > " << b;
+    a > b ? cout << " -> True" : cout << " -> False";
+
+    cout << '\n'  << b << " < " << a;
+    b < a ? cout << " -> True" : cout << " -> False";
+
+    cout << '\n'  << b << " > " << a;
+    b > a ? cout << " -> True" : cout << " -> False";
+
+    cout << '\n';
+
+    system("pause");
+
+    return 0;
+}
